6ae8bd3a9965efeb39d5c604e1b60cf27f561062
Bruno Daniel <frandalozzo@gmail.com>
2021-11-24 13:22:10 -0300

    aula2

---
+++ Exercicio5/Calculo.cs
@@ -0,0 +1,55 @@
+using System;
+namespace Exercicio5
+{
+    public class Calculo
+    {
+        public double  IR(double salario)
+        {
+            double total = 0;
+            double descIR = 0;
+            if (salario <= 2000 || salario == 2000)
+            {   
+                total = 0;
+                Console.WriteLine("você é isento de IR");
+            }
+            if (salario <= 5000)
+            {
+                descIR = 5.0 / 100.0;
+                total = salario - (descIR * salario);
+                total = salario - total;
+            }
+            if (salario <= 7500)
+            {
+                descIR = 10.0 / 100.0;
+                total = salario - (descIR * salario);
+                total = salario - total;
+                
+            }
+            if (salario > 15000)
+            {
+                descIR = 20.0 / 100.0;
+                total = salario - (descIR * salario);
+                total = salario - total;
+                
+            }
+             return total ;
+        }
+        public double INSS(double salario)
+        {
+            double total;
+            double descINSS = 10.0 / 100.0;
+
+            total = salario - (descINSS * salario);
+            total = salario - total;
+            return total;
+        }
+        public double FGTS(double salario)
+        {
+            double total;
+            double descFGTS = 11.0 / 100.0;
+            total = salario - (descFGTS * salario);
+            total = salario - total;
+            return total;
+        }
+    }
+}
