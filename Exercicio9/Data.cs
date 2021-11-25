69cb985e8de9b7249505d852f4f1cdcf076a5351
Bruno Daniel <frandalozzo@gmail.com>
2021-11-25 09:53:08 -0300

    att

--- Exercicio9/Data.cs
+++ Exercicio9/Data.cs
@@ -8,10 +8,10 @@ namespace Exercicio9
                 if(mes >= 1 && mes <= 12){
                     if(dia >= 1 && dia <= 31){
                         if(mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12)
-                            Console.WriteLine("data válida meu cupixa");
+                            Console.WriteLine("dia válido meu cupixa");
                     }
                     if((dia >= 1 && dia <=30) && (mes == 4 || mes == 6 || mes == 9 || mes == 11))
-                            Console.WriteLine("data valida meu cupixa");
+                            Console.WriteLine("dia valido meu cupixa");
                     if((dia >= 1 && dia <=28) && (mes == 2))
                         Console.WriteLine("dia valido de fevereiro");
                     else 
