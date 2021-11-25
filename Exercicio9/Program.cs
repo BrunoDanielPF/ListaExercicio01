c1e05ac531a5e93573aa22be84a1a3646a5a0ada
Bruno Daniel <frandalozzo@gmail.com>
2021-11-25 09:49:55 -0300

    exec finalizado

--- Exercicio9/Program.cs
+++ Exercicio9/Program.cs
@@ -6,7 +6,19 @@ namespace Exercicio9
     {
         static void Main(string[] args)
         {
-            
+            /*Faça um Programa que peça uma data no formato 
+            dd/mm/aaaa e determine se a mesma é uma data válida. */
+           Console.WriteLine("infomr a data no formato dd/mm/aaaa para verificar a validade"); 
+           Console.WriteLine("dia ? ");
+           int dia = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("mes? ");
+           int mes = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("ano? ");
+           int ano = Convert.ToInt32(Console.ReadLine());
+
+            Data verificador = new Data();
+            verificador.VerificaData(dia,mes,ano);
+
         }
     }
 }
